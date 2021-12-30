6a6542477446e5646da064bdbf06211abe9720eb
BenPaulat <benjamin.d.paulat@gmail.com>
2021-12-29 21:30:29 -0600

    added virtual to thought schema and exported model

--- models/Thought.js
+++ models/Thought.js
@@ -17,5 +17,20 @@ const ThoughtSchema = new Schema(
             required: true
         },
         reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
 );
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
