da427ac95c4faedfa8d74ebf02dc30a02160ac1d
BenPaulat <benjamin.d.paulat@gmail.com>
2021-12-30 16:38:40 -0600

    completed update user controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -45,6 +45,14 @@ const userController = {
     // update user by id
     updateUser({ params, body }, res) {
         User.findOneAndUpdate({ _id: params.id}, body, { new: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
     },
 
     // delete user
