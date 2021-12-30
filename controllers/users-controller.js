39852ac80e8c82240b69a65d39609a0360473cc4
BenPaulat <benjamin.d.paulat@gmail.com>
2021-12-30 16:36:41 -0600

    completed create user controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -38,6 +38,8 @@ const userController = {
     // create user
     createUser({ body }, res) {
         User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
     },
 
     // update user by id
