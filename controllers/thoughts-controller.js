ba30baeaf3cdfd0e39db5b33cd2dce479c8eabcc
BenPaulat <benjamin.d.paulat@gmail.com>
2021-12-30 16:45:39 -0600

    completed get thought by id controller

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -16,6 +16,17 @@ const thoughtController = {
     // get one thought by id
     getThoughtById({ params }, res) {
         Thought.findOne({ _id: params.thoughtId })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
 
     // create thought
