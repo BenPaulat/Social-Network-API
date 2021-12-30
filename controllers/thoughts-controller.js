176239e106d7a221b0fa1482f5f61d9a4dd96ed5
BenPaulat <benjamin.d.paulat@gmail.com>
2021-12-30 17:19:54 -0600

    completed create thought controller in thoughtController

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -30,8 +30,23 @@ const thoughtController = {
     },
 
     // create thought
-    createThought({ body }, res) {
+    createThought({ params, body }, res) {
         Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
     },
 
     // update thought by id
