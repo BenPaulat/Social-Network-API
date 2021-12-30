cb9378eabd251439ec1a6f51fbc1975e11a86842
BenPaulat <benjamin.d.paulat@gmail.com>
2021-12-30 15:21:35 -0600

    setup basic structure for user controller

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,14 +1,15 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
+
     // get all thoughts
     getThoughts(req, res) {
-        Thought.find()
+        Thought.find({})
         .sort({ createdAt: -1 })
         .then((dbThoughtData) => res.json(dbThoughtData))
         .catch(err => {
             console.log(err);
-            res.status(500).json(err);
+            res.status(400).json(err);
         })
     },
     // get one thought by id
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,1 +1,33 @@
 // normal CRUD + addFriend/RemoveFriend
+
+const { User } = require('../models');
+
+const userController = {
+
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+    },
+
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+    },
+
+    // create user
+    createUser({ body }, res) {
+        User.create(body)
+    },
+
+    // update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id}, body, { new: true })
+    },
+
+    // delete user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+    }
+};
+
+module.exports = userController;
